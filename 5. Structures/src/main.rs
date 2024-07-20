db1af88f0f5e1af2bc7e689eb9a3188f12c1874c
mrmeowlnir <dmsdens@gmail.com>
2024-07-20 18:59:39 +0300

    5.3. Methods

--- 5. Structures/src/main.rs
+++ 5. Structures/src/main.rs
@@ -4,6 +4,25 @@ struct Rectangle{
     height: u32,
 }
 
+impl Rectangle {
+    // Методы принимают &self в качестве аргумента
+    fn area(&self) -> u32{
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool{
+        self.width > other.width && self.height > other.height
+    }
+
+    // Ассоциированные функции не принимают &self в качестве аргумента
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     let scale = 2;
     let rect = Rectangle{
@@ -12,9 +31,26 @@ fn main() {
     };
     dbg!(&rect);
 
-    println!("The area of {rect:?} is {}", area(&rect));
+    println!("The area of {rect:?} is {}", rect.area());
+
+    let rect1 = Rectangle{
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    let sq = Rectangle::square(50);
+
+    println!("{rect1:?} can hold {rect2:?}? {}", rect1.can_hold(&rect2));
+    println!("{rect1:?} can hold {rect3:?}? {}", rect1.can_hold(&rect3));
+    println!("{rect1:?} can hold {sq:?}? {}", rect1.can_hold(&sq));
 }
 
-fn area(rectangle: &Rectangle) -> u32{
-    rectangle.width * rectangle.height
-}
+
