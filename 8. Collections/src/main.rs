2d06bfe06a4bac91af126327f7ed5711ee00aaf0
mrmeowlnir <dmsdens@gmail.com>
2024-07-27 14:57:23 +0300

    8 Collections, start with Vec<T>

---
+++ 8. Collections/src/main.rs
@@ -0,0 +1,40 @@
+#![allow(dead_code)]
+fn main() {
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {third}");
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element."),
+    }
+
+    let mut v2 = vec![100, 32, 57];
+    for i in &mut v2 {
+        *i += 50;
+    }
+    for i in &v2 {
+        println!("{i}");
+    }
+
+    #[derive(Debug)]
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+
+    for i in &row{
+        println!("{i:#?} ");
+    }
+    println!("\n");
+
+}
