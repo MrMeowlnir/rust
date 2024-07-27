8da98d63ac4f5460d27c8a44ff7af6c831f3f489
mrmeowlnir <dmsdens@gmail.com>
2024-07-27 15:33:15 +0300

    8 Collections, added strings and hashmaps

--- 8. Collections/src/main.rs
+++ 8. Collections/src/main.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 fn main() {
+    println!("Vec<T>");
     let v = vec![1, 2, 3, 4, 5];
 
     let third: &i32 = &v[2];
@@ -32,9 +33,63 @@ fn main() {
         SpreadsheetCell::Float(10.12),
     ];
 
-    for i in &row{
-        println!("{i:#?} ");
+    for i in &row {
+        println!("{i:?}\n");
     }
-    println!("\n");
+    println!("\n\n");
+
+    println!("Strings");
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s1 is {s1}");
+    println!("s2 is {s2}");
+
+    for c in "Зд".chars() {
+        println!("{c}");
+    }
+
+    for b in "Зд".bytes() {
+        println!("{b}");
+    }
+    println!("\nHash-maps\n");
+
+    use std::collections::HashMap;
+    {
+        println!("Init:");
+        let mut scores = HashMap::new();
+
+        scores.insert(String::from("Blue"), 10);
+        scores.insert(String::from("Yellow"), 50);
+
+        for (key, value) in &scores {
+                println!("{key}: {value}");
+        }
+    }
+
+    {
+        println!("or_insert:");
+        let mut scores = HashMap::new();
+        scores.insert(String::from("Blue"), 10);
+
+        scores.entry(String::from("Yellow")).or_insert(50);
+        scores.entry(String::from("Blue")).or_insert(50);
+
+        println!("{scores:?}");
+    }
+    {
+        println!("Update");
+        let text = "hello world wonderful world";
+
+        let mut map = HashMap::new();
+
+        for word in text.split_whitespace() {
+            let count = map.entry(word).or_insert(0);
+            *count += 1;
+        }
+
+        println!("{map:?}");
+    }
 
 }
